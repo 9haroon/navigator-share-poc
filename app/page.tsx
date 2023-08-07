b46dd8475b2e39a1cc830a3072eec466907b360f
haroonth <kittisak.ma@logicspark.com>
2023-08-07 11:21:37 +0700

    ✨ POC web share using navigator.share

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,70 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import Image from "next/image";
+import styles from "./page.module.css";
+import { FormEvent, useCallback, useRef } from "react";
+
+async function webShare(text: string, file?: File | null) {
+	try {
+		if (navigator) {
+			console.log("text", text);
+			await navigator.share({
+				title: text,
+				text: text,
+				...(file && { files: [file] }),
+			});
+		}
+	} catch (error) {
+		console.log(error);
+		alert("Your device cannot share this");
+	}
+}
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	const formRef = useRef<HTMLFormElement>(null);
+	const handleSubmit = useCallback(
+		(ev: FormEvent) => {
+			ev.preventDefault();
+			const form = formRef.current;
+			const shareTextElem = form?.querySelector(
+				'[name="shareText"]'
+			) as HTMLTextAreaElement;
+			const shareImageElem = form?.querySelector(
+				'[name="shareImage"]'
+			) as HTMLInputElement;
+			console.log("shareImageElem", shareTextElem.value, shareImageElem.files);
+			// const imageFile = new File(shareImageElem.value as unknown as any, { })
+			webShare(shareTextElem.value, shareImageElem.files?.item(0));
+		},
+		[formRef]
+	);
+	return (
+		<main className={styles.main}>
+			<div>
+				<form
+					suppressHydrationWarning={true}
+					ref={formRef}
+					onSubmit={handleSubmit}>
+					<p>
+						<label>
+							Image{" "}
+							<input
+								type="file"
+								name="shareImage"
+								accept="image/*"
+							/>
+						</label>
+					</p>
+					<p>
+						<label>
+							Text
+							<textarea
+								name="shareText"
+								rows={5}></textarea>
+						</label>
+					</p>
+					<button type="submit">Share</button>
+				</form>
+			</div>
+		</main>
+	);
 }
